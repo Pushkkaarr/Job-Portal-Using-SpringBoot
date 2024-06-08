164958b8d679fe4f91378c39646c4b4f9d13bb1d
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2024-06-08 11:12:36 +0530

    Trial Hello World

--- src/main/java/com/pushkar/JobApp/JobAppApplication.java
+++ src/main/java/com/pushkar/JobApp/JobAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JobAppApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
